832cfd1d6a44dacb64dc15f95ee5786d839e9f93
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-29 15:51:27 +0300

    delete 1 todo

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,11 @@ function App() {
        {id:2, title:"JS", isDone: false },
        {id:3, title:"React", isDone: false }
    ]
-    const task2 =[
-        {id:1, title:"Hello World!", isDone: true },
-        {id:2, title:"Yo", isDone: false },
-        {id:3, title:"Manchester City", isDone: true }
-    ]
 
 
     return (
         <div className="App">
            <Todolist title="What to learn" tasks={task1}/>
-           <Todolist title="Repeat" tasks={task2}/>
         </div>
     );
 }
