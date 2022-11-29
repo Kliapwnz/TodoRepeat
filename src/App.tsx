dcd7adfea3d904d7b85cfc48e6a83373fd8181ec
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-29 15:44:20 +0300

    add tasks

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,22 @@ import './App.css';
 import {Todolist} from "./Todolist";
 
 function App() {
+   const task1 =[
+       {id:1, title:"Html&CSS", isDone: true },
+       {id:2, title:"JS", isDone: false },
+       {id:3, title:"React", isDone: false }
+   ]
+    const task2 =[
+        {id:1, title:"Hello World!", isDone: true },
+        {id:2, title:"Yo", isDone: false },
+        {id:3, title:"Manchester City", isDone: true }
+    ]
+
+
     return (
         <div className="App">
-           <Todolist/>
+           <Todolist title="What to learn" tasks={task1}/>
+           <Todolist title="Repeat" tasks={task2}/>
         </div>
     );
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
-export const Todolist = () => {
+type PropsType={
+    title:string
+    tasks: tasksPropsType[]
+}
+type tasksPropsType={
+    id: number
+    title:string
+    isDone: boolean
+}
+
+export const Todolist = (props:PropsType) => {
     return (
             <div>
-                <h3>What to learn</h3>
+                <h3>{props.title}</h3>
                 <div>
                     <input/>
                     <button>+</button>
