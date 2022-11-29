194f545014a9ff9fd27f27da4e8df21201f30492
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-29 15:50:28 +0300

    add map

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,34 +1,34 @@
 import React from 'react';
 
-type PropsType={
-    title:string
+type PropsType = {
+    title: string
     tasks: tasksPropsType[]
 }
-type tasksPropsType={
+type tasksPropsType = {
     id: number
-    title:string
+    title: string
     isDone: boolean
 }
 
-export const Todolist = (props:PropsType) => {
+export const Todolist = (props: PropsType) => {
     return (
+        <div>
+            <h3>{props.title}</h3>
             <div>
-                <h3>{props.title}</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
-                    <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
-                    <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
+                <input/>
+                <button>+</button>
             </div>
+            <ul>
+                {props.tasks.map(el => <li key={el.id}><input type="checkbox" checked={el.isDone}/>
+                    <span>{el.title}</span></li>
+                )}
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
     );
 };
 
